8e4115c4aaff5c51d5e0194a55aba81ef7f8fc61
Leung <alexander.leung@intel.com>
2016-07-15 11:01:15 -0700

    fibonacci

--- solutions/pythonProjects/pythonProjects/fibonacci.py
+++ solutions/pythonProjects/pythonProjects/fibonacci.py
@@ -2,6 +2,7 @@
 # 0 1 1 2 3 5 8 13
 # 0 1 2 3 4 5 6 7
 
+'''
 n = int(input("Nth fibonacci number to go to: "))
 
 first = 0
@@ -12,9 +13,23 @@ for i in range(1,n):
     second += first
     first = third
     print("{} ".format(second))
+'''
 
 def fib(n):
-    series[1]
+    
+    assert n > 0
+    series = [1]
+
+    while len(series) < n:
+        if len(series) == 1:
+            series.append(1)
+        else:
+            series.append(series[-1] + series[-2])
+
+    for i in range(len(series)):
+        series[i] = str(series[i])
+
+    return(', '.join(series) + '.')
 
 def main():
     print(fib(int(input("Nth fibonacci number to go to: "))))
